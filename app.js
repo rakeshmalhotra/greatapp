8a6bf48ee1a635e64a9e3ef3e32123c95088acef
rakesh malhotra <rakesh.3043@gmail.com>
2017-05-20 12:44:25 +0530

    Ok

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express')
-var app = express()
+var app = express.createServer();
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
+app.listen(8080, function () {
   console.log('Example app listening on port 3000!')
 })
